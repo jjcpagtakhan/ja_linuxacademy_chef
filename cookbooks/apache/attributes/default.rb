20685acb58e6be6fbd37ec0b30e4bef74141bd32
root <root@lazam1.mylabserver.com>
2017-09-18 10:04:51 +0000

    Commit chef repo for thr first time

---
+++ cookbooks/apache/attributes/default.rb
@@ -0,0 +1,10 @@
+default["apache"]["sites"]["lazam2"] = { "site_title" => "Anthony2 websites coming soon", "port" => 80, "domain" => "lazam2.mylabserver.com"}
+default["apache"]["sites"]["lazam2b"] = { "site_title" => "Anthony2b websites coming soon!","port" => 80, "domain" => "lazam2b.mylabserver.com"}
+default["apache"]["sites"]["lazam3"] = { "site_title" => "Anthony3 websites coming soon!","port" => 80, "domain" => "lazam3.mylabserver.com"}
+
+case node["platform"]
+when "centos"
+	default["apache"]["package"] = "httpd"
+when "ubuntu"
+	default["apache"]["package"] = "apache2"
+end
