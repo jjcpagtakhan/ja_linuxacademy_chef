e812dfce53bdba6f5fae220f72a30b188b25d816
root <root@lazam1.mylabserver.com>
2017-09-19 10:32:46 +0000

    Added environment for dev

--- cookbooks/apache/attributes/default.rb
+++ cookbooks/apache/attributes/default.rb
@@ -2,6 +2,8 @@ default["apache"]["sites"]["lazam2"] = { "site_title" => "Anthony2 websites comi
 default["apache"]["sites"]["lazam2b"] = { "site_title" => "Anthony2b websites coming soon!","port" => 80, "domain" => "lazam2b.mylabserver.com"}
 default["apache"]["sites"]["lazam3"] = { "site_title" => "Anthony3 websites coming soon!","port" => 80, "domain" => "lazam3.mylabserver.com"}
 
+default["author"]["name"] = "janine"
+
 case node["platform"]
 when "centos"
 	default["apache"]["package"] = "httpd"
---
+++ environments/dev.rb
@@ -0,0 +1,3 @@
+name "dev"
+description "This is the development environment"
+cookbook "apache", "= 0.1.0"
